782c17c06851a77eca7478d3e1efe7f636228e04
wanglijun <juns1984@qq.com>
2016-12-27 22:56:33 +0800

    创建网络

--- zk/start-zk.sh
+++ zk/start-zk.sh
@@ -5,7 +5,7 @@ START_ZK2='docker run --restart always --network starfish --ip 192.0.2.2 -d --en
 
 START_ZK3='docker run --restart always --network starfish --ip 192.0.2.3 -d --env  ZOO_MY_ID=3 --name zk3 --env ZOO_SERVERS="server.1=zk1:2888:3888 server.2=zk2:2888:3888 server.3=zk3:2888:3888" zookeeper:3.4.9'
 
-ssh in-205-bj $START_ZK1
-ssh in-204-bj $START_ZK2
-ssh in-902-gd $START_ZK3
+ssh in-205-gd $START_ZK1
+ssh in-204-gd $START_ZK2
+ssh in-902-bj $START_ZK3
 
