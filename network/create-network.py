910c8bf3684a11147e49aba05126c578bac2a003
wanglijun <juns1984@qq.com>
2016-12-28 15:11:50 +0800

    网段地址部署

--- network/create-network.py
+++ network/create-network.py
@@ -5,22 +5,35 @@ import os, sys
 
 def subnet(name):
     if name == "zookeeper":
-        return "192.0.2.0/24"
+        return "192.0.2.0"
     elif name == "orgman":
-        return "192.0.3.0/24"
+        return "192.0.3.0"
     elif name == "qida":
-        return "192.0.4.0/24"
+        return "192.0.4.0"
     elif name == "lecai":
-        return "192.0.5.0/24"
+        return "192.0.5.0"
     else:
         raise ValueError("unknown net:" + name)
 
 
+def create_pool_yaml(name):
+    spec = \
+"""- apiVersion: v1
+  kind: ipPool
+  metadata:
+    cidr: {subnet}/24
+""".format(subnet=subnet(name))
+    fd = open("ippool-{name}.yaml".format(name=name), "w")
+    fd.write(spec)
+    fd.close()
+
+
 def create_cali_net(name):
+    create_pool_yaml(name)
     cmd_list = [
         "calicoctl create -f ippool-{name}.yaml".format(name=name),
-        "calicoctl apply -f policy-{name}.yaml".format(name=name),
-        "docker network create --driver calico --ipam-driver calico-ipam --subnet{subnet} {name}".format(
+        "calicoctl apply -f policy-full-connect.yaml".format(name=name),
+        "docker network create --driver calico --ipam-driver calico-ipam --subnet={subnet}/24 {name}".format(
             name=name,
             subnet=subnet(name)
         )
--- network/remove-network.py
+++ network/remove-network.py
@@ -1,14 +1,38 @@
 #!/usr/bin/env python
-import os,sys
+import os, sys
+
+
+def subnet(name):
+    if name == "zookeeper":
+        return "192.0.2.0"
+    elif name == "orgman":
+        return "192.0.3.0"
+    elif name == "qida":
+        return "192.0.4.0"
+    elif name == "lecai":
+        return "192.0.5.0"
+    else:
+        raise ValueError("unknown net:" + name)
+
+
+def create_pool_yaml(name):
+    spec = \
+""" - apiVersion: v1
+  kind: ipPool
+  metadata:
+    cidr: {subnet}/24
+""".format(subnet=subnet(name))
+    fd = open("ippool-{name}.yaml".format(name=name), "w")
+    fd.write(spec)
+    fd.close()
 
 def delete_cali_net(name):
+    create_pool_yaml(name)
     cmd_list = [
-        "docker network rm {name}".format(name = name),
-        "calicoctl delete -f ippool-{name}.yaml".format(name=name),
-        "calicoctl delete -f policy-{name}.yaml".format(name=name)
+        "docker network rm {name}".format(name=name),
+        "calicoctl delete -f ippool-{name}.yaml".format(name=name)
     ]
     [os.system(cmd) for cmd in cmd_list]
-
 
 if __name__ == "__main__":
     nets = sys.argv[1:]
