f1536dc0263fbb685fb6d52892d7a5c945db137d
wanglijun <juns1984@qq.com>
2016-12-28 12:50:03 +0800

    部署master

---
+++ maxwell/deploy-master.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+__author__ = 'junix'
+
+import sys, os, getopt
+
+
+def docker_cmd(gid, pid):
+    return \
+        "docker run --restart always \
+        --network starfish \
+        --ip 192.0.2.{index} \
+        --env GROUP_ID={gid} \
+        --env NODE_ID={pid} \
+        -d \
+        junix/maxwell_master".format(gid=gid, index=pid+10, pid=pid)
+
+def ssh_cmd(host, cmd):
+    return "ssh {host} '{cmd}'".format(host=host, cmd=cmd)
+
+
+if __name__ == "__main__":
+    optlist, hosts = getopt.getopt(sys.argv[1:], 'g:')
+    if hosts == [] or optlist == []:
+        print("usage: cmd --group_id=gid hosts")
+        sys.exit(1)
+    gid = dict(optlist).get('-g')
+    for index, host in enumerate(hosts):
+        pid = index + 1
+        cmd = ssh_cmd(host, docker_cmd(gid, pid))
+        os.system(cmd)
