e6b61582edbe6973ee4896066d28a2dd1c8707a3
wanglijun <juns1984@qq.com>
2016-12-28 15:47:01 +0800

    网段地址部署

--- maxwell/deploy-orgman.py
+++ maxwell/deploy-orgman.py
@@ -3,15 +3,18 @@ __author__ = 'junix'
 
 import sys, os, getopt
 
+def zk_addr():
+    return os.getenv("ZOOKEEPER",'192.0.2.1:2181,192.0.2.2:2181,192.0.2.3:2181')
 
-def docker_cmd(gid, pid):
+def docker_cmd(pid):
     return \
         "docker run --restart always \
-        --network starfish \
-        --ip 192.0.2.{index} \
-        --env NODE_ID={pid} \
+        --network orgman \
+        --ip 192.0.3.{index} \
+        --env NODE_ID={node_id} \
+        --env ZOOKEEPER={zk} \
         -d \
-        junix/orgman".format(gid=gid, index=20 + pid, pid=pid)
+        junix/orgman".format(index = pid, node_id=pid, zk=zk_addr())
 
 
 def ssh_cmd(host, cmd):
@@ -19,12 +22,11 @@ def ssh_cmd(host, cmd):
 
 
 if __name__ == "__main__":
-    optlist, hosts = getopt.getopt(sys.argv[1:], 'g:')
+    optlist, hosts = getopt.getopt(sys.argv[1:], 'n:')
     if hosts == [] or optlist == []:
-        print("usage: cmd --group_id=gid hosts")
         sys.exit(1)
-    gid = int(dict(optlist).get('-g'))
+    nid = int(dict(optlist).get('-n'))
     for index, host in enumerate(hosts):
         pid = index + 1
-        cmd = ssh_cmd(host, docker_cmd(gid, pid))
+        cmd = ssh_cmd(host, docker_cmd(nid))
         os.system(cmd)
