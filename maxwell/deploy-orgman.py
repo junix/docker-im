b3ae26d61edad2e8736473c16d81fbaa06216c3e
wanglijun <juns1984@qq.com>
2016-12-28 15:50:13 +0800

    网段地址部署

--- maxwell/deploy-orgman.py
+++ maxwell/deploy-orgman.py
@@ -3,8 +3,14 @@ __author__ = 'junix'
 
 import sys, os, getopt
 
+
 def zk_addr():
-    return os.getenv("ZOOKEEPER",'192.0.2.1:2181,192.0.2.2:2181,192.0.2.3:2181')
+    return os.getenv("ZOOKEEPER", '192.0.2.1:2181,192.0.2.2:2181,192.0.2.3:2181')
+
+
+def org_msg_mq():
+    return os.getenv("KAFKA_TOPIC", 'org_message')
+
 
 def docker_cmd(pid):
     return \
@@ -13,13 +19,17 @@ def docker_cmd(pid):
         --ip 192.0.3.{index} \
         --env NODE_ID={node_id} \
         --env ZOOKEEPER={zk} \
+        --env KAFKA_TOPIC={mq} \
         -d \
-        junix/orgman".format(index = pid, node_id=pid, zk=zk_addr())
+        junix/orgman".format(
+            index=pid,
+            node_id=pid,
+            zk=zk_addr(),
+            mq=org_msg_mq())
 
 
 def ssh_cmd(host, cmd):
     return "ssh {host} '{cmd}'".format(host=host, cmd=cmd)
-
 
 if __name__ == "__main__":
     optlist, hosts = getopt.getopt(sys.argv[1:], 'n:')
@@ -29,4 +39,5 @@ if __name__ == "__main__":
     for index, host in enumerate(hosts):
         pid = index + 1
         cmd = ssh_cmd(host, docker_cmd(nid))
+        print(cmd)
         os.system(cmd)
