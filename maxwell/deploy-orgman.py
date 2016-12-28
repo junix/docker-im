025b64eee3a069d1f2a364d5cb8762aac2b2333c
wanglijun <juns1984@qq.com>
2016-12-28 15:52:07 +0800

    网段地址部署

--- maxwell/deploy-orgman.py
+++ maxwell/deploy-orgman.py
@@ -34,6 +34,7 @@ def ssh_cmd(host, cmd):
 if __name__ == "__main__":
     optlist, hosts = getopt.getopt(sys.argv[1:], 'n:')
     if hosts == [] or optlist == []:
+        print("usage:cmd -n nid host_to_deploy")
         sys.exit(1)
     nid = int(dict(optlist).get('-n'))
     for index, host in enumerate(hosts):
