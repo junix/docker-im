6527666b436edffdf1d9d3bd426732cf5fe75d20
wanglijun <juns1984@qq.com>
2016-12-30 15:32:59 +0800

    add kafka deply

--- kafka/entry.sh
+++ kafka/entry.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
+if [[ "x$ZOOKEEPER" == "x" ]]
+then
+    export ZOOKEEPER="192.0.2.1:2181,192.0.2.2:2181,192.0.2.3:2181,192.0.2.4:2181,192.0.2.5:2181"
+fi
+
 if [[ "x$BROKER_ID" == "x" ]]
 then
     echo "BROKER_ID not set"
     exit 1
 fi
 
-if [[ "x$ZOOKEEPER" == "x" ]]
-then
-    ZOOKEEPER="192.0.2.1:2181,192.0.2.2:2181,192.0.2.3:2181,192.0.2.4:2181,192.0.2.5:2181"
-fi
 
 sed -i "s/{{BROKER_ID}}/${BROKER_ID}/g" /app/config/*
 sed -i "s/{{ZOOKEEPER}}/${ZOOKEEPER}/g" /app/config/*
