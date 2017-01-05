ce616b3aa1a727940b8299d3369f52c02cd0e83e
wanglijun <juns1984@qq.com>
2017-01-05 23:06:48 +0800

    deploy backend

--- maxwell/deploy-frontend.py
+++ maxwell/deploy-frontend.py
@@ -17,6 +17,14 @@ def env_or_error(env_key):
         return v
 
 
+def env_or(env_key, def_val):
+    v = os.getenv(env_key)
+    if v is None:
+        return def_val
+    else:
+        return v
+
+
 def zk_env():
     zk = env_or_nil("ZOOKEEPER")
     return '' if zk == '' else '--env ZOOKEEPER={zk}'.format(zk=zk)
@@ -37,11 +45,15 @@ def docker_cmd(gid, pid):
         --network host \
         --env GROUP_ID={gid} \
         --env NODE_ID={pid} \
+        --env EXTERNAL_IP={external_ip} \
+        --env EXTERNAL_PORT={external_port} \
         {zk_env} \
         --name={name}fg{gid}p{pid}\
         -d \
         junix/maxwell_frontend".format(
         gid=gid,
+        external_port=env_or("EXTERNAL_PORT", "2013"),
+        external_ip=env_or_error("EXTERNAL_IP"),
         name=name_prefix(),
         zk_env=zk_env(),
         pid=pid)
@@ -56,6 +68,8 @@ def usage():
     print("""usage:deploy-frontend hosts
     env: ZOOKEEPER default 192.0.2.[1-5]:2181
          GROUP_ID
+         EXTERNAL_PORT default 2013
+         EXTERNAL_IP
          NAME_PREFIX default nil""")
 
 
