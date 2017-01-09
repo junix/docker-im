b18975836f235c2b018ad3f1bc928664a1a269c8
wanglijun <juns1984@qq.com>
2017-01-09 15:42:10 +0800

    deploy convstore

--- maxwell/deploy-convstore.py
+++ maxwell/deploy-convstore.py
@@ -9,9 +9,11 @@ import utils
 class ConvStoreCmd(docker_cmd.DockerCmd):
     def __init__(self, ip_offset, node_id):
         docker_cmd.DockerCmd.__init__(self)
+        name_prefix = os.getenv('NAME_PREFIX', 'convstore')
         node_ip = '192.0.7.{index}'.format(index=ip_offset + node_id)
         self.use_image('junix/conv_store').daemon_mode(). \
             with_network(net='conv_store', ip=node_ip). \
+            with_name('{prefix}{id}'.format(prefix=name_prefix, id=node_id)). \
             with_os_env('ZOOKEEPER', utils.zk_env(1, 5)). \
             with_env('NODE_ID', node_id)
 
