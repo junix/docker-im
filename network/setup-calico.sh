47f4c4aa0da4710bfe6f9b07a1156bd207b16031
wanglijun <juns1984@qq.com>
2017-01-12 12:57:40 +0800

    install docker

--- network/setup-calico.sh
+++ network/setup-calico.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ETCD_ENDPOINTS=${ETCD_ENDPOINTS:-'http://10.10.92.203:2379,http://10.10.93.174:2379,http://10.10.86.15:2379,http://10.10.94.51:2379,http://10.10.95.147:2379,http://10.10.90.194:2379,http://10.10.86.12:2379'}
 PROGRAM=/usr/local/bin/calicoctl
 cp ./calicoctl ${PROGRAM}
 chmod +x $PROGRAM
