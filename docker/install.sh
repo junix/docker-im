05f20f4315bfc4452127223da26de64d085dbe87
wanglijun <juns1984@qq.com>
2017-01-12 10:54:11 +0800

    install docker

---
+++ docker/install.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+if [[ "$MYIP" == "" ]]; then
+    echo 'env MYIP must set'
+fi
+
+function install_repo_file {
+    REPO=/etc/yum.repos.d/docker.repo
+    echo '[dockerrepo]'> ${REPO}
+    echo 'name=Docker Repository' >> ${REPO}
+    echo 'baseurl=https://yum.dockerproject.org/repo/main/centos/7/' >> ${REPO}
+    echo 'enabled=1' >> ${REPO}
+    echo 'gpgcheck=1' >> ${REPO}
+    echo 'gpgkey=https://yum.dockerproject.org/gpg' >> ${REPO}
+}
+
+function install_docker() {
+    yum install -y docker-engine
+}
+
+function conf_docker() {
+
+    mkdir /etc/systemd/system/docker.service.d
+    CONF='/etc/systemd/system/docker.service.d/docker.conf'
+
+    echo '[Service]' > ${CONF}
+    echo 'EnvironmentFile=-/etc/sysconfig/docker' >> ${CONF}
+    echo 'EnvironmentFile=-/etc/sysconfig/docker-storage' >> ${CONF}
+    echo 'EnvironmentFile=-/etc/sysconfig/docker-network' >> ${CONF}
+    echo 'ExecStart=' >> ${CONF}
+    echo 'ExecStart=/usr/bin/dockerd $OPTIONS $DOCKER_STORAGE_OPTIONS $DOCKER_NETWORK_OPTIONS $BLOCK_REGISTRY $INSECURE_REGISTRY' >> ${CONF}
+
+    SYS_CONF=/etc/sysconfig/docker
+    echo "OPTIONS=\"-g /mnt1/docker --log-opt max-size=10m --log-opt max-file=10 -H tcp://${MYIP}:2375 \
+-H unix:///var/run/docker.sock --cluster-store=${ETCD_URL:-etcd://-127.0.0.1:4001} --cluster-advertise=${MYIP}:2375\"" > ${SYS_CONF}
+}
+
+function systemctl_docker() {
+    systemctl enable docker.service
+}
+
+install_repo_file &&
+systemctl_docker &&
+conf_docker &&
+systemctl start docker
