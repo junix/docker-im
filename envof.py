b42e1a4550296a4c48a82ec5f650f054e3cca8e4
wanglijun <juns1984@qq.com>
2017-01-14 13:50:33 +0800

    addd ipof.sh scripts

--- envof.py
+++ envof.py
@@ -8,4 +8,7 @@ if __name__ == '__main__':
     if len(containers) != 1:
         print('usage:envof.py container')
         sys.exit(1)
-    print(utils.env_of_container(containers[0]))
+
+    env_list = utils.env_of_container(containers[0])
+    for e in env_list:
+        print(e)
