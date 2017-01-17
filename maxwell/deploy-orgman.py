c59d95ca3f4ab0b7c0aa2eb1809dad2ac7d21979
wanglijun <juns1984@qq.com>
2017-01-17 10:35:07 +0800

    add mountof

--- maxwell/deploy-orgman.py
+++ maxwell/deploy-orgman.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 import getopt
 import sys
+import os
 
 from docker_cmd import DockerCmd
+import utils
 
 
 class OrgmanCmd(DockerCmd):
-
     def __init__(self, pid, offset):
         DockerCmd.__init__(self)
         self.pid = pid
-        self.use_image('junix/orgman').daemon_mode(). \
-            with_network(network='orgman', ip='192.0.3.{index}'.format(index=offset + pid)). \
-            with_env('NODE_ID', pid).copy_os_env('ZOOKEEPER'). \
-            copy_os_env('KAFKA_TOPIC').with_mount_from_env('DATA_DIR', '/app/data'). \
+        ip = utils.ip_of('orgman', pid + offset)
+        name = '{prefix}{pid}'.format(
+            prefix=os.getenv('NAME_PREFIX', 'orgman'),
+            pid=pid)
+        self.use_image('junix/orgman').\
+            daemon_mode(). \
+            with_name(name).\
+            with_network(network='orgman', ip=ip). \
+            with_env('NODE_ID', pid).\
+            copy_os_env('ZOOKEEPER'). \
+            copy_os_env('KAFKA_TOPIC').\
+            with_mount_from_env('DATA_DIR', '/app/data'). \
             with_mount_from_env('LOG_DIR', '/app/log')
 
 
@@ -25,8 +34,8 @@ if __name__ == "__main__":
         print("env:   ZOOKEEPER=192.0.2.[1-5]:2181")
         print("       KAFKA_TOPIC")
         sys.exit(1)
-    start_from = int(dict(optlist).get('-n', '1'))
-    ip_offset = int(dict(optlist).get('-f', '0'))
+    start_from = int(dict(optlist).get('-n', '0'))
+    ip_offset = int(dict(optlist).get('-f', '1'))
     for index, host in enumerate(hosts):
         pid = index + start_from
         c = OrgmanCmd(offset=ip_offset, pid=pid)
