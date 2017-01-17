328b9adfc5a89218e178c32c30c892b77281b1b6
wanglijun <juns1984@qq.com>
2017-01-17 11:17:30 +0800

    add mountof

--- maxwell/deploy-convstore.py
+++ maxwell/deploy-convstore.py
@@ -10,10 +10,11 @@ class ConvStoreCmd(docker_cmd.DockerCmd):
     def __init__(self, ip_offset, node_id):
         docker_cmd.DockerCmd.__init__(self)
         name_prefix = os.getenv('NAME_PREFIX', 'convstore')
+        name = '{prefix}{id}'.format(prefix=name_prefix, id=node_id)
         node_ip = utils.ip_of('conv_store', ip_offset + node_id)
         self.use_image('junix/conv_store').daemon_mode(). \
             with_network(network='conv_store', ip=node_ip). \
-            with_name('{prefix}{id}'.format(prefix=name_prefix, id=node_id)). \
+            with_name(name).\
             copy_os_env('ZOOKEEPER', utils.zk_env()). \
             with_env('NODE_ID', node_id)
 
@@ -23,8 +24,8 @@ if __name__ == "__main__":
     if not hosts:
         print("usage:cmd hosts")
         sys.exit(1)
-    offset = int(dict(optlist).get('-f', '0'))
+    offset = int(dict(optlist).get('-f', '1'))
     for index, host in enumerate(hosts):
-        pid = index + 1
-        c = ConvStoreCmd(ip_offset=offset, node_id=pid)
-        c.exec_in(host).execute(dryrun='--dryrun' in dict(optlist).keys())
+        ConvStoreCmd(ip_offset=offset, node_id=index).\
+            exec_in(host).\
+            execute(dryrun='--dryrun' in dict(optlist).keys())
