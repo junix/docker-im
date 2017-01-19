eeff4d2b60d59c4f1d4a239d69c318c69291cd87
wanglijun <juns1984@qq.com>
2017-01-19 13:27:45 +0800

    install-svc-run

--- kafka/deploy-prod.sh
+++ kafka/deploy-prod.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
-env DATA_DIR=/mnt2/kafka/data LOG_DIR=/mnt2/kafka/log KAFKA_HEAP_OPTS="-Xms1G -Xmx4G" ./deploy-kafka.py --dryrun 10.10.53.26 10.10.60.240 10.10.39.177
+env DATA_DIR=/mnt2/kafka/data \
+LOG_DIR=/mnt2/kafka/log \
+KAFKA_HEAP_OPTS="-Xms1G -Xmx4G" \
+./deploy-kafka.py --dryrun 10.10.53.26 10.10.60.240 10.10.39.177
