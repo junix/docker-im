e576250cfb01ed9b9e8eab2ade076b22e1084d50
wanglijun <juns1984@qq.com>
2017-01-19 12:53:06 +0800

    install-svc-run

--- etcd/new-dev.sh
+++ etcd/new-dev.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
 rm -rvf /mnt1/etcd/*
-etcd -name $(hostname) \
+
+INIT_CLUSTER="\
+i821-bj=http://10.10.31.56:2380,\
+i822-bj=http://10.10.53.26:2380,\
+i823-bj=http://10.10.60.240:2380,\
+i824-bj=http://10.10.39.177:2380,\
+i825-bj=http://10.10.38.126:2380"
+
+INTERNAL_IP=${INTERNAL_IP:127.0.0.1}
+
+INIT_CMD="etcd -name $(hostname) \
   --data-dir /mnt1/etcd \
   -initial-advertise-peer-urls http://${INTERNAL_IP}:2380 \
   -listen-peer-urls http://${INTERNAL_IP}:2380 \
   -listen-client-urls http://${INTERNAL_IP}:2379,http://127.0.0.1:4001 \
   -advertise-client-urls http://${INTERNAL_IP}:2379 \
   -initial-cluster-token etcd-cluster-starfish \
-  -initial-cluster \
-i821-bj=http://10.10.31.56:2380,\
-i822-bj=http://10.10.53.26:2380,\
-i823-bj=http://10.10.60.240:2380,\
-i824-bj=http://10.10.39.177:2380,\
-i825-bj=http://10.10.38.126:2380\
-  -initial-cluster-state new>/dev/null
+  -initial-cluster ${INIT_CLUSTER} \
+  -initial-cluster-state new>/dev/null"
+
+bash -c ${INIT_CMD}
