5ba15a954fd9083f14c6e69c7cf3731bd6688c3d
wanglijun <juns1984@qq.com>
2017-01-19 10:25:57 +0800

    install-svc-run

--- etcd/install-svc-monitor.sh
+++ etcd/install-svc-monitor.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cp svc-run.template svc-run
 sed -i "s/__MYIP__/${INTERNAL_IP}/g" svc-run
-END_EXPR=$(echo $ETCD_ENDPOINTS | sed 's/\//\\\//g')
+ETCD_EXPR=$(echo $ETCD_ENDPOINTS | sed 's/\//\\\//g')
 sed -i "s/__ETCD_ENDPOINTS__/${ETCD_EXPR}/g" svc-run
 mkdir /service/etcd
 chmod +x ./svc-run
