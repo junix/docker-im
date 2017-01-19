a406914adf352a40dd7fb7024b65b49a025feb49
wanglijun <juns1984@qq.com>
2017-01-19 11:28:15 +0800

    install-svc-run

---
+++ network/clear-iptables.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+ipt="iptables"
+$ipt -P INPUT ACCEPT
+$ipt -P FORWARD ACCEPT
+$ipt -P OUTPUT ACCEPT
+$ipt -F
+$ipt -X
+$ipt -t nat -F
+$ipt -t nat -X
+$ipt -t mangle -F
+$ipt -t mangle -X
+$ipt -t raw -X
+$ipt -t raw -F
+echo "ok"
