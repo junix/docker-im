b4391099803073d0ca4147a7c284abb51d466392
wanglijun <juns1984@qq.com>
2017-01-20 22:21:00 +0800

    restart

--- maxwell/deploy-master.py
+++ maxwell/deploy-master.py
@@ -11,34 +11,31 @@ class MasterCmd(DockerCmd):
 
     def __init__(self, node_id, ip_offset):
         DockerCmd.__init__(self)
-        name_prefix = os.getenv('NAME_PREFIX', 'm')
-        group_id = int(os.getenv("GROUP_ID", '0'))
         node_ip = utils.ip_of('master', ip_offset + node_id)
+        name = 'mg{gid}p{pid}'.format(gid=os.getenv('GROUP_ID'), pid=node_id)
         self.use_image('yunxuetang/maxwell_master').daemon_mode(). \
             with_network(network='master', ip=node_ip). \
-            with_name('{prefix}g{gid}p{pid}'.format(prefix=name_prefix, gid=group_id, pid=node_id)). \
+            with_name(name).\
             copy_os_env('ZOOKEEPER', utils.zk_env()). \
             copy_os_env('GROUP_ID', can_ignore=False). \
             with_env('NODE_ID', node_id)
 
 
 def usage():
-    print("""usage:deploy-master hosts
+    print('''usage:deploy-master [-f | --dryrun] hosts
     env: ZOOKEEPER default 192.0.2.[1-5]:2181
          GROUP_ID
-         NAME_PREFIX default nil""")
+         NAME_PREFIX default nil''')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     optlist, hosts = getopt.getopt(sys.argv[1:], 'f:', ['dryrun'])
     if not hosts:
         usage()
         sys.exit(1)
-    offset = int(dict(optlist).get('-f', '1'))
+    options = dict(optlist)
+    offset = int(options.get('-f', '1'))
     for index, host in enumerate(hosts):
-        c = MasterCmd(node_id=index, ip_offset=offset)
-        c.exec_in(host)
-        if '--dryrun' in dict(optlist).keys():
-            c.show()
-        else:
-            c.execute()
+        MasterCmd(node_id=index, ip_offset=offset).\
+            exec_in(host).\
+            execute('--dryrun' in options)
