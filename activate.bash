cb4e6bb5697edcec85f04e93f82f7ccef7e0b189
wanglijun <juns1984@qq.com>
2017-01-20 14:20:39 +0800

    restart

--- activate.bash
+++ activate.bash
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
-export PATH=`pwd`:${PATH}
-export PATH=`pwd`/container:${PATH}
+PROJ=/opt/mos/codebase/docker-im
+export PATH=${PROJ}/container:${PATH}
+export PATH=${PROJ}/network:${PATH}
+export PATH=${PROJ}/meaxwell:${PATH}
+export PATH=${PROJ}/zk:${PATH}
+export PATH=${PROJ}/kafka:${PATH}
