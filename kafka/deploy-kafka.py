3ab0c0e8506781ede86adc27a6fb8383d182380e
wanglijun <juns1984@qq.com>
2017-01-21 22:46:06 +0800

    use `IP_OFFSET` env to specify ip offset

--- kafka/deploy-kafka.py
+++ kafka/deploy-kafka.py
@@ -12,18 +12,16 @@ class KafkaCmd(DockerCmd):
     def __init__(self, broker_id):
         DockerCmd.__init__(self)
         self.node_id = broker_id
+        name = 'kafka{pid}'.format(pid=self.node_id)
         self.use_image('yunxuetang/kafka:2.9.1').\
             daemon_mode().\
-            with_name(self.instance_name()).\
+            with_name(name).\
             copy_os_env('ZOOKEEPER', utils.zk_env()). \
             copy_os_env('KAFKA_HEAP_OPTS', '-Xms1G -Xmx4G'). \
             with_env('BROKER_ID', broker_id). \
             with_network(network='kafka', ip=utils.ip_of('kafka', broker_id)). \
             with_mount_from_env('DATA_DIR', '/app/data'). \
             with_mount_from_env('LOG_DIR', '/app/logs')
-
-    def instance_name(self):
-        return '{prefix}{pid}'.format(prefix=os.getenv('NAME_PREFIX', 'kafka'), pid=self.node_id)
 
 
 def usage():
