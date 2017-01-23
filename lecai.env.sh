7b44613be6530ba7d60505b0b6e67578c2265188
wanglijun <juns1984@qq.com>
2017-01-23 16:47:03 +0800

    add id_offset

--- lecai.env.sh
+++ lecai.env.sh
@@ -2,6 +2,6 @@
 export ZOOKEEPER='192.0.2.101:2181,192.0.2.102:2181,192.0.2.103:2181,192.0.2.104:2181,192.0.2.105:2181'
 export NAME_PREFIX='lecai_'
 export GROUP_ID=1
-export IP_OFFSET=100
+export IP_OFFSET=101
 export KAFKA_TOPIC='lecai_org_message'
 export PUBLISH_TOPIC='lecai_conv_sinker'
