daf1ea06a47da7cacf59a70f4244d0fa9d18fb11
wanglijun <juns1984@qq.com>
2017-01-25 10:30:01 +0800

    deploy backend

--- qida.env.sh
+++ qida.env.sh
@@ -2,5 +2,6 @@
 export ZOOKEEPER='192.0.2.1:2181,192.0.2.2:2181,192.0.2.3:2181,192.0.2.4:2181,192.0.2.5:2181'
 export GROUP_ID=0
 export IP_OFFSET=1
+export NAME_PREFIX=
 export KAFKA_TOPIC='org_message'
 export PUBLISH_TOPIC='conv_sinker'
